e1edd398e8ccc5fc59e701c385a4ac7ad6de647a
iseyoshitaka <iseyoshitaka@team-lab.com>
2021-01-06 20:25:37 +0900

    APIのエラーメッセージと、レスポンスコードを設定した。

--- web-front/src/main/java/com/isystk/sample/web/front/controller/api/v1/auth/AuthRestController.java
+++ web-front/src/main/java/com/isystk/sample/web/front/controller/api/v1/auth/AuthRestController.java
@@ -7,12 +7,15 @@ import com.isystk.sample.domain.entity.TUser;
 import com.isystk.sample.web.base.controller.api.AbstractRestController;
 import com.isystk.sample.web.base.controller.api.resource.Resource;
 import com.isystk.sample.web.front.dto.auth.AuthUserDto;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
 
 import static com.isystk.sample.common.FrontUrl.*;
 
@@ -79,10 +82,10 @@ public class AuthRestController extends AbstractRestController {
    * @return
    */
   @GetMapping(API_V1_LOGIN_FAILURE_URL)
-  public Resource loginFailure() {
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  public Resource loginFailure(HttpServletResponse response) {
     Resource resource = resourceFactory.create();
     resource.setMessage(getMessage("login.failed"));
-
     return resource;
   }
 
